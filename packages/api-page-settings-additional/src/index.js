6b0f379b0c130b7f420c43b58719cb86c8df442f
Roman Vabishchevych <r@vabishchevych.com>
2020-04-24 02:53:09 +0300

    fix: lerna test23

--- packages/api-page-settings-additional/src/index.js
+++ packages/api-page-settings-additional/src/index.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-server-lambda";
 import { withFields, string, fields } from "@webiny/commodo";
 
-if (window.DEBUG) console.log("test22");
+if (window.DEBUG) console.log("test23");
 
 export default () => [
   {
