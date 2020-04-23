c7dec772c86678accdec149b9cbd8d2ccb5fe106
Roman Vabishchevych <r@vabishchevych.com>
2020-04-24 01:27:49 +0300

    chore: lerna test4

--- packages/api-page-settings-additional/src/index.js
+++ packages/api-page-settings-additional/src/index.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-server-lambda";
 import { withFields, string, fields } from "@webiny/commodo";
 
-if (window.DEBUG) console.log("test3");
+if (window.DEBUG) console.log("test4");
 
 export default () => [
   {
