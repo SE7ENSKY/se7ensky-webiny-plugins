837b90c1018f3760ff011edc0aa13c83073e52b3
Roman Vabishchevych <r@vabishchevych.com>
2020-04-24 05:04:18 +0300

    test: lerna 33

--- packages/api-page-settings-additional/src/index.js
+++ packages/api-page-settings-additional/src/index.js
@@ -1,7 +1,7 @@
 import { gql } from "apollo-server-lambda";
 import { withFields, string, fields } from "@webiny/commodo";
 
-if (window.DEBUG) console.log("test32");
+if (window.DEBUG) console.log("test33");
 
 export default () => [
   {
